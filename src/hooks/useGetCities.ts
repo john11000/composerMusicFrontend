b34543f074e45c28fc088e0811c2c9e6ad323e02
john11000 <86936709+john11000@users.noreply.github.com>
2023-10-18 13:43:47 -0500

    Update useGetCities.ts

--- src/hooks/useGetCities.ts
+++ src/hooks/useGetCities.ts
@@ -1,12 +1,11 @@
 import { URL_API_CITIES } from "@/constants/url-apis.constants";
 import useFetch from "./useFetch";
-import { IDepartments } from "@/features/customers/models/Customers.type";
 
 export default function useGetDepartaments() {
   const { request, loading } = useFetch();
 
   const getDepartaments = () => {
-    return request<IDepartments[]>({ url: URL_API_CITIES });
+    return request<any>({ url: URL_API_CITIES });
   };
 
   return { getDepartaments, loading };
